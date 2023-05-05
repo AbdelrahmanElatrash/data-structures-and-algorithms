605b8aa53b9edc5618d17ef217b7a057448a1104
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-05-05 21:27:25 +0000

    stack and queue

---
+++ linked_list/linked_list_2.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+
+    def reverse(self):
+        prev_node = None
+        current_node = self.head
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head = prev_node
+
+
+
+
+def reverse(self):
+        if self.head is None or self.head.next is None:
+            return self.head
+        rest = LinkedList()
+        rest.head = self.head.next
+        rest.reverse()
+        self.head.next.next = self.head
+        self.head.next = None
+        self.head = rest.head
---
+++ stack_and_queue/readme.md
@@ -0,0 +1,24 @@
+# stack and queue 
+
+## Whiteboard Process
+
+[stack and queue](./witebord.pdf)
+
+
+## Approach & Efficiency
+
+ouer code explain how is the queue and stack  defined and how they are work as a datastructure
+
+create your env 
+`python -m venv .venv`
+
+install dependance
+`pip3 install pytest`
+
+
+
+## test
+in test directory run
+`pytest`
+
+[test solution](./test)
